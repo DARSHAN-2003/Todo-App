e64c33d0b31189fd9a70d8a33c63936f6e1e19ae
DARSHAN-2003 <darshandeulkar03@gmail.com>
2024-08-05 23:45:19 +0530

    initial commit

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,15 +1,4 @@
-// app/api/tasks/route.js
-// import fs from 'fs';
-// import path from 'path';
 
-// export async function GET() {
-//   const filePath = path.join(process.cwd(), 'data', 'tasks.json');
-//   const jsonData = fs.readFileSync(filePath);
-//   const tasks = JSON.parse(jsonData);
-//   return new Response(JSON.stringify(tasks), {
-//     headers: { 'Content-Type': 'application/json' },
-//   });
-// }/
 
 import fs from 'fs';
 import path from 'path';
